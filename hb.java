df0c2ffd98d31af9f9b2dac490f9c8550959a579
Gauri Dnyaneshwar Bhagyawant <gaurib124>
2024-02-22 23:27:50 +0530

    file added

---
+++ hb.java
@@ -0,0 +1,25 @@
+import java util.*;
+public class TowerOfHanoi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println((int)Math.pow(2,n)-1);
+        System.out.println("Gauri")
+        solve(n, 1 ,3,2)
+
+    }
+    public static void solve( int n, int from, int tc,int aux)
+    {
+        int(n==1)
+        {
+            System.out.println(from + " " + to);
+            return;
+        }
+        solve(n - 1, from, aux, to);
+        System.out.println(from + " " + to);
+        solve(n-1,aux, tofrom)
+        di
+    }
+}
